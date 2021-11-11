eb3b766aa8106e3e00393c69345de71e728d0ef4
elia-dot <eliacohen100@gmail.com>
2021-11-11 21:21:17 +0200

    added populate to goal

--- models/Saving.js
+++ models/Saving.js
@@ -27,7 +27,7 @@ savingSchema.post('save', async function () {
   await user.save();
 });
 
-savingSchema.pre('save', async function (next) {
+savingSchema.pre(/^find/, function (next) {
   this.populate('target');
   next();
 });
