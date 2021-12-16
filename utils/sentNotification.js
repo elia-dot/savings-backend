73f009dcbad29c8ccff4c6f3ae1dab6fc97bd515
elia-dot <eliacohen100@gmail.com>
2021-12-16 17:28:02 +0200

    added try catch to send notification

--- utils/sentNotification.js
+++ utils/sentNotification.js
@@ -32,7 +32,7 @@ module.exports.handlePushTokens = async (push, req, res) => {
       body: body,
       data: { body },
     });
-  
+
     let chunks = expo.chunkPushNotifications(notifications);
     let tickets = [];
     (async () => {
@@ -47,8 +47,48 @@ module.exports.handlePushTokens = async (push, req, res) => {
       }
     })();
   } catch (error) {
-    console.log("error: ", error);
+    console.log('error: ', error);
   }
 
-  
+  let receiptIds = [];
+  for (let ticket of tickets) {
+    // NOTE: Not all tickets have IDs; for example, tickets for notifications
+    // that could not be enqueued will have error information and no receipt ID.
+    if (ticket.id) {
+      receiptIds.push(ticket.id);
+    }
+  }
+
+  let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+  (async () => {
+    // Like sending notifications, there are different strategies you could use
+    // to retrieve batches of receipts from the Expo service.
+    for (let chunk of receiptIdChunks) {
+      try {
+        let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+        console.log(receipts);
+
+        // The receipts specify whether Apple or Google successfully received the
+        // notification and information about an error, if one occurred.
+        for (let receiptId in receipts) {
+          let { status, message, details } = receipts[receiptId];
+          if (status === 'ok') {
+            continue;
+          } else if (status === 'error') {
+            console.error(
+              `There was an error sending a notification: ${message}`
+            );
+            if (details && details.error) {
+              // The error codes are listed in the Expo documentation:
+              // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+              // You must handle the errors appropriately.
+              console.error(`The error code is ${details.error}`);
+            }
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
 };
