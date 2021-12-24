41154bdc3fb129892a1488a9634f0eec934ee57f
elia-dot <eliacohen100@gmail.com>
2021-12-24 16:27:18 +0200

    added i18n

---
+++ bin/addProfit.js
@@ -0,0 +1,36 @@
+#! /app/.heroku/node/bin/node
+const Child = require('../models/Child');
+const Saving = require('../models/Saving');
+const date = new Date();
+
+const addProfit = async () => {
+  try {
+    console.log('addindg profit');
+    const childs = await Child.find({ revenue: { $gt: 0 } });
+    
+    childs.forEach((child) => {
+      const total = child.saving + child.profit;
+      const profit = total * (child.revenue / 100);
+      child.profit += profit;
+      child.save();
+
+      const createSaving = async () => {
+        await Saving.create({
+          user: child._id,
+          amount: profit,
+          description: 'monthly profit',
+        });
+      };
+      createSaving();
+    });
+    console.log('profit added');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (date.getDate() === 24) {
+  addProfit();
+} else {
+  console.log('Not today');
+}
